507f5e53d732cdad8bc8653d52d7e6c5766b3e2c
aschkenasy <eli@themodularway.com>
2019-05-19 19:08:32 -0400

    added drop multiple tables snippet - only thing that needs to be changed is the table name pattern (line 5)

---
+++ drop_multiple_tables.sql
@@ -0,0 +1,9 @@
+SET group_concat_max_len = 5000;
+SELECT CONCAT('DROP TABLE ', GROUP_CONCAT(table_name), ';')
+       INTO @droplike
+FROM information_schema.tables
+WHERE table_name LIKE 'pattern_%';
+
+PREPARE stmt FROM @droplike;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
