538c042dcce482cb6b76b0ae63cc0aaa2f326368
aschkenasy <eli@themodularway.com>
2019-05-19 19:10:35 -0400

    added replace prefixes of multiple tables snippet - table name pattern (line 5), and new prefix (line 2) have to be changed

---
+++ replace_multiple_table_prefixes.sql
@@ -0,0 +1,9 @@
+SET group_concat_max_len = 5000;
+SELECT CONCAT('RENAME TABLE ', GROUP_CONCAT(table_name, ' TO ', REPLACE(table_name, 'pattern_', 'new_prefix')), ';')
+       INTO @renametables
+FROM information_schema.tables
+WHERE table_name LIKE 'pattern_%';
+
+PREPARE stmt FROM @renametables;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
